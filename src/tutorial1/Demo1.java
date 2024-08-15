c882c053f044f454e4873a8ea970e5da71cec374
surajpeddawad <surajpeddawad77@gmail.com>
2024-08-15 23:17:34 +0530

    Demo 5 TC by Pranav

--- src/tutorial1/Demo1.java
+++ src/tutorial1/Demo1.java
@@ -3,7 +3,7 @@ package tutorial1;
 public class Demo1 {
 
 	public static void main(String[] args) {
-
+		System.out.println("Demo 1 Testcase by TL");
 	}
 
 }
---
+++ src/tutorial2/Demo5.java
@@ -0,0 +1,10 @@
+package tutorial2;
+
+public class Demo5 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo 5 Testcase by Pranav");
+
+	}
+
+}
